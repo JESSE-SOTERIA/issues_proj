39c3f07c6bcbdfcda01b6a6cb28e28246d8f88e7
Jesse Lawrence <soterialawrence009@gmail.com>
2024-09-09 12:36:31 +0300

    scaffold

---
+++ issues_proj/lib/issues_proj.ex
@@ -0,0 +1,18 @@
+defmodule IssuesProj do
+  @moduledoc """
+  Documentation for `IssuesProj`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IssuesProj.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ issues_proj/test/issues_proj_test.exs
@@ -0,0 +1,8 @@
+defmodule IssuesProjTest do
+  use ExUnit.Case
+  doctest IssuesProj
+
+  test "greets the world" do
+    assert IssuesProj.hello() == :world
+  end
+end
